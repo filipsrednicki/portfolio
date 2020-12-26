5e01dfd5ded0b9892d3d8cb174d45535fee50a97
Filip <filipsrednicki42@gmail.com>
2020-12-26 21:27:26 +0100

    Replaced one of the projects with a new one

--- src/Projects.js
+++ src/Projects.js
@@ -10,46 +10,47 @@ class Projects extends Component {
     openedProject: null,
     projects: [
       {
+        title: 'Release Calendar',
+        url: 'https://releases-cd8a7.web.app/',
+        img: '/portfolio/img/release-calendar.jpg',
+        delay: 100,
+        description: 'This app is built using React with Hooks and Context API. It\'s using Firebase Auth for creating and managing users and Realtime Database for storing and managing user\'s data. It\'s also using two API\'s: TMDB for fetching data about movies and tv shows; RAWG for games. Users can search for information about movies, tv shows and games. Once logged in, they can add them to their calendar to keep track of the release date.',
+        additionalInfo: 'TEST ACCOUNT: e-mail - test@test.com, password - test123'
+      },
+      {
         title: 'Neighborhood Map',
         url: 'https://filipsrednicki.github.io/neighborhood-map/',
         img: '/portfolio/img/neighborhood-map.jpg',
-        delay: 100,
+        delay: 200,
         description: 'Single-page application built using React with addition of GoogleMapsAPI. Functionality of this application includes: map markers, a search function and a list view to support simple browsing of all locations. As well as using FoursquareAPI to provide additional information about each of these locations.'
       },
       {
         title: 'Arcade Game: Frogger',
         url: 'https://filipsrednicki.github.io/arcade-game-frogger/',
         img: '/portfolio/img/arcade-game.jpg',
-        delay: 200,
+        delay: 300,
         description: 'I was provided visual assets and a game loop engine, using these tools I had to add a number of entities to the game including the player character and enemies. Additional functionality: collectibles, score, levels, winning/losing screen.'
       },
       {
         title: 'Memory Game',
         url: 'https://filipsrednicki.github.io/memory-game/',
         img: '/portfolio/img/memory-game.jpg',
-        delay: 300,
+        delay: 400,
         description: 'The Memory Game Project is all about demonstrating my mastery of HTML, CSS, and JavaScript. I built a browser-based card matching game (also known as Concentration). But this isn’t just any memory game! It’s a shnazzy, well-designed, feature-packed memory game!'
       },
       {
         title: 'MyReads',
         url: 'https://filipsrednicki.github.io/myreads/',
         img: '/portfolio/img/myreads.jpg',
-        delay: 400,
+        delay: 500,
         description: 'In this project, I had to create a bookshelf app (using React) that allows a user to select and categorize books he/she has read, are currently reading, or want to read. I was provided with an API server and client library that\'s used to persist information as user interacts with the application.'
       },
       {
         title: 'Restaurant Reviews',
         url: 'https://filipsrednicki.github.io/mws-restaurant-stage-1/',
         img: '/portfolio/img/restaurant-reviews.jpg',
-        delay: 500,
-        description: 'I was provided code for a restaurant reviews website. The code had a lot of issues. It was barely usable on a desktop browser, much less a mobile device. It didn’t include any accessibility features and work offline at all. My task was to resolve these issues while still maintaining the included functionality.'
-      },
-      {
-        title: 'Simple Portfolio',
-        url: 'https://filipsrednicki.github.io/simple-portfolio/',
-        img: '/portfolio/img/simple-portfolio.jpg',
         delay: 600,
-        description: 'Being provided with a design mockup as a PDF-file my task was to replicate that design in HTML and CSS.  I developed a responsive website that displays images, descriptions and links to each of the portfolio projects.'
+        description: 'I was provided code for a restaurant reviews website. The code had a lot of issues. It was barely usable on a desktop browser, much less a mobile device. It didn’t include any accessibility features and work offline at all. My task was to resolve these issues while still maintaining the included functionality.'
       }
     ]
   }
@@ -116,6 +117,7 @@ class Projects extends Component {
                   <img src={this.state.openedProject.img} className='modal-img' alt={this.state.openedProject.title}/>
                   <h1>{this.state.openedProject.title}</h1>
                   <p className='modal-text'>{this.state.openedProject.description}</p>
+                  <p className='modal-text'>{this.state.openedProject.additionalInfo}</p>
                 </div>
                 <div className='modal-buttons'>
                   <a href={this.state.openedProject.url} target="_blank"><span className='mb-view' role='button'>View site</span></a>
